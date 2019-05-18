ddf49e866378d4ce78aa4f78a30b607d95d92584
xuzhounan <1219142922@qq.com>
2019-05-18 12:33:38 +0800

    test1

---
+++ test1_xzn/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.IO;
+using System.ComponentModel;
+using System.Drawing;
+using GeometryGym.Ifc;
+using GeometryGym.STEP;
+
+namespace test1_xzn
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal IfcBridgeStructureType mPredefinedType = IfcBridgeStructureType.ARCHED_BRIDGE;    //:	IfcBridgeStructureType;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".ifc";
+            dlg.Filter = " (.ifc)|*.ifc";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                fileTextBox.Text = filename;
+            }
+        }
+
+    }
+}
